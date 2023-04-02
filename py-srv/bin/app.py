edbf88310b9a127c4d6191804f9daef722b451c7
bearddan2000 <bearddan2000@yahoo.com>
2023-04-01 23:16:46 -0400

    initial commit

---
+++ py-srv/bin/app.py
@@ -0,0 +1,10 @@
+from node import Node
+
+es_node = Node()
+es_node.range_query()
+es_node.regex_query()
+es_node.must_not_query()
+es_node.must_query()
+es_node.must_filter_query()
+es_node.filter_query()
+es_node.get_all_query()
